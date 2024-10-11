59985d9180abc5cca3a835aa6aa70efae30029b9
Zian <147041958+Lootchy@users.noreply.github.com>
2024-10-11 21:16:14 +0200

    Update Alive.h

--- ExoEncpasulation/ExoEncpasulation/Alive.h
+++ ExoEncpasulation/ExoEncpasulation/Alive.h
@@ -1,25 +1,19 @@
 #pragma once
-#include <iostream>
 
 class Alive {
-private :
-	float MaxHealth;
-	float CurrentHealth;
+private:
+    float MaxHealth;
+    float CurrentHealth;
 
-public :
-	Alive() : MaxHealth(100.0f), CurrentHealth(MaxHealth){}
-	Alive(float MaxHealthValue, float CurrentHealthValue) : MaxHealth(MaxHealthValue), CurrentHealth(MaxHealth) {}
+public:
+    // Constructeurs
+    Alive();
+    Alive(float MaxHealthValue, float CurrentHealthValue);
 
-	float GetMaxHealth(float MaxHealthValue) {
-		return MaxHealth;
-	}
+    // Getters
+    float GetMaxHealth() const;
+    float GetCurrentHealth() const;
 
-	float GetCurrentHealth(float CurrentHealthValue) {
-		return CurrentHealth;
-	}
-
-	virtual void GetDamage(float CurrentHealthValue, float damage) = 0 {
-		CurrentHealth = CurrentHealthValue - damage;
-	}
-
+    // Méthode pour recevoir des dégâts
+    virtual void GetDamage(float damage);
 };
