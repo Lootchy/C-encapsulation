fdfc976ed64aa671ed4cdef0470c15820df47b7d
Zian <147041958+Lootchy@users.noreply.github.com>
2024-10-11 21:17:37 +0200

    Update Int.h

--- ExoEncpasulation/ExoEncpasulation/Int.h
+++ ExoEncpasulation/ExoEncpasulation/Int.h
@@ -1,6 +1,5 @@
 #pragma once
 #include <iostream>
-#include <cmath>
 
 class Int {
 private:
@@ -8,82 +7,27 @@ private:
 
 public:
     // Constructeurs
-    Int() : Integer(0) {}
-    Int(int _int) : Integer(_int) {}
+    Int();
+    Int(int _int);
 
-    // Surcharge des opérateurs arithmétiques
-    Int operator+(const Int& rv) const {
-        std::cout << "operator+" << std::endl;
-        return Int(Integer + rv.Integer);
-    }
+    // Surcharge des opÃ©rateurs arithmÃ©tiques
+    Int operator+(const Int& rv) const;
+    Int operator+=(const Int& rv);
+    Int operator-(const Int& rv) const;
+    Int operator-=(const Int& rv);
+    Int operator*(const Int& rv) const;
+    Int operator*=(const Int& rv);
+    Int operator/(const Int& rv) const;
+    Int operator/=(const Int& rv);
+    Int operator%(const Int& rv) const;
+    Int operator%=(const Int& rv);
 
-    Int operator+=(const Int& rv) {
-        std::cout << "operator+=" << std::endl;
-        Integer += rv.Integer;
-        return *this;
-    }
+    // MÃ©thode pour calculer la puissance
+    Int Puissance(int exposant) const;
 
-    Int operator-(const Int& rv) const {
-        std::cout << "operator-" << std::endl;
-        return Int(Integer - rv.Integer);
-    }
+    // Getter pour accÃ©der Ã  l'attribut Integer
+    int getValue() const;
 
-    Int operator-=(const Int& rv) {
-        std::cout << "operator-=" << std::endl;
-        Integer -= rv.Integer;
-        return *this;
-    }
-
-    Int operator*(const Int& rv) const {
-        std::cout << "operator*" << std::endl;
-        return Int(Integer * rv.Integer);
-    }
-
-    Int operator*=(const Int& rv) {
-        std::cout << "operator*=" << std::endl;
-        Integer *= rv.Integer;
-        return *this;
-    }
-
-    Int operator/(const Int& rv) const {
-        std::cout << "operator/" << std::endl;
-        return Int(Integer / rv.Integer);
-    }
-
-    Int operator/=(const Int& rv) {
-        std::cout << "operator/=" << std::endl;
-        Integer /= rv.Integer;
-        return *this;
-    }
-
-    Int operator%(const Int& rv) const {
-        std::cout << "operator%" << std::endl;
-        return Int(Integer % rv.Integer);
-    }
-
-    Int operator%=(const Int& rv) {
-        std::cout << "operator%=" << std::endl;
-        Integer %= rv.Integer;
-        return *this;
-    }
-
-    // Méthode pour calculer la puissance
-    Int Puissance(int exposant) const {
-        int resultat = std::pow(Integer, exposant);
-        return Int(resultat);
-    }
-
-    // Getter pour accéder à l'attribut Integer
-    int getValue() const {
-        return Integer;
-    }
-
-    // Surcharge de l'opérateur <<
+    // Surcharge de l'opÃ©rateur <<
     friend std::ostream& operator<<(std::ostream& os, const Int& rv);
 };
-
-// Implémentation de la surcharge de l'opérateur <<
-std::ostream& operator<<(std::ostream& os, const Int& rv) {
-    os << rv.Integer; // Affiche la valeur de l'entier
-    return os;
-}
